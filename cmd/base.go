59653bd77c92b733cf2ce3b5abde22a5e2f78d97
Guilherme Nogara <guilherme.nogara@dotgroup.com.br>
2020-05-26 14:17:22 -0300

    Adicionando comando para limpar imagens docker

--- cmd/base.go
+++ cmd/base.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/urfave/cli"
 )
@@ -13,6 +14,22 @@ func SetupBase(c []cli.Command) []cli.Command {
 		Usage:   "Show a hello world command line",
 		Action: func(c *cli.Context) {
 			fmt.Println("Hello World")
+		},
+	})
+
+	c = append(c, cli.Command{
+		Name:     "docker-clean",
+		Category: "Docker",
+		Usage:    "Cleans docker enviroment",
+		Action: func(c *cli.Context) {
+			command := exec.Command("docker", "stop $(docker ps -aq) && docker rm $(docker ps -qa) && docker network prune -f")
+			output, erro := command.CombinedOutput()
+			if erro != nil {
+				fmt.Println(fmt.Sprint(erro) + ": " + string(output))
+				return
+			}
+			fmt.Println("Docker is now clean")
+
 		},
 	})
 
